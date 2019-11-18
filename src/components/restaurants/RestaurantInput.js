4556723280c21c032399c984a801ee108a9b6a11
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-11-17 19:15:00 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+
+const initialState = {
+  text: ''
+}
 
 class RestaurantInput extends Component {
+
+  state = initialState
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })  
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState(initialState)
+  }
+
   render() {
     return (
       <div>
         Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type='text'
+            value={this.state.text}
+            onChange={this.handleOnChange}
+          />
+          <input type='submit'/>
+        </form>
       </div>
-    );
+    )
   }
-};
+}
 
-export default RestaurantInput;
+export default RestaurantInput
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import App from './App'
 import manageRestaurant from './reducers/manageRestaurant'
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 const store = createStore(manageRestaurant)
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>,
-document.getElementById('root')
+  document.getElementById('root')
 )
